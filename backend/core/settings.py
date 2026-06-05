3c81876722a3b4bec3e9f2eeb692c6b66372bc7d
Th0Kh4nh <khanh14042004@gmail.com>
2026-06-05 09:15:48 +0700

    postgre

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
 # ==============================================================================
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://https://4-man-sport.vercel.app"
+    "https://4-man-sport.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 

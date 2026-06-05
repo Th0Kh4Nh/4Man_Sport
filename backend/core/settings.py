60e5bcf70de806cc2677436dd6744a87314a2ba3
Th0Kh4nh <khanh14042004@gmail.com>
2026-06-05 08:42:38 +0700

    Cap nhat CORS cho Vercel

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,9 +80,8 @@ MIDDLEWARE = [
 # CORS CONFIGURATION (cho Frontend ReactJS)
 # ==============================================================================
 CORS_ALLOWED_ORIGINS = config(
-    'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173',
-    cast=Csv()
+    "http://localhost:5173",
+    "https://4-man-sport.vercel.app"
 )
 CORS_ALLOW_CREDENTIALS = True
 
